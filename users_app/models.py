71bf136c56aaa1bccdd40816ba65aaf1435b6ca1
Sam Branka <sdbranka@me.com>
2021-05-21 16:39:22 -0400

    in process

--- users_app/models.py
+++ users_app/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class users(models.Model):
+class Movie(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email_address = models.CharField(max_length=255)
     age = models.IntegerField()
+    description = models.TextField()
+    release_date = models.DateTimeField()
+    duration = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
