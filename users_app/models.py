7d0992c1117a20d4aaab1a680911c55a9cc36294
Sam Branka <sdbranka@me.com>
2021-05-21 20:42:05 -0400

    complete

--- users_app/models.py
+++ users_app/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Movie(models.Model):
+class users(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email_address = models.CharField(max_length=255)
     age = models.IntegerField()
-    description = models.TextField()
-    release_date = models.DateTimeField()
-    duration = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"<users object: {self.first_name}, {self.last_name}, {self.email_address}, {self.age}>"
